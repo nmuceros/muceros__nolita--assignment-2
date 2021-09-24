2d2a15c72310911012b578cbac98ab547e3302d5
nmuceros <nolita.muceros@gmail.com>
2021-09-23 23:49:50 -0400

    adds error message when entered an empty task

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 import Todo from "./Todo";
 import "../../src/App.css"
+
 // import deleteIcon from '../images/delete.svg';
 
 const TodoList = () => {
     /* Hooks */
     const [todo, setTodo] = useState(''); 
     const [todos, setTodos] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    // const validation = value => {
+    //     let errors='';
+        
+    //     if(!todo.entered_todo) {
+    //         errors="Please enter a new task!";
+    //     }
+    //     return errors;
+    // }
 
     const changeTodo_Handler = (e) => {
         setTodo(e.target.value);
+        console.log(e.target.value)
+        setErrorMessage(!errorMessage)
+
+        // setErrors(!errors.todo)
         // setTodo((prevState) => ({
         //     ...prevState, 
         //     [e.target.name]: e.target.value,
@@ -18,13 +33,25 @@ const TodoList = () => {
 
     const submitTodo_Handler = (e) => {
         e.preventDefault(); /* prevents state to return to its default state */
-        setTodos([...todos, {
-                    entered_todo: todo, 
-                    completed: false, 
-                    id: Math.random() * 1000,
-                
-                }]);
-        setTodo(""); /* set the state to its default value */
+        // setErrors(validation(e.target.value));
+        if(!todo) {
+            setErrorMessage("Please enter a task first!")
+
+        }
+        else {
+
+        
+ 
+            setTodos([...todos, {
+                entered_todo: todo, 
+                completed: false, 
+                id: Math.random() * 1000,
+
+            
+            }]);
+            setTodo(""); /* set the state to its default value */
+          
+        }
     }
     
     
@@ -39,7 +66,9 @@ const TodoList = () => {
                         placeholder="Enter new task here..." 
                         onChange={ changeTodo_Handler} 
                         autoFocus
-                        value = { todo } />
+                        value = { todo } 
+                    />
+                    {errorMessage && <p className="error">{errorMessage}</p>}
                 </form>
             </div>
             {/* <div className="sticky-here">Sticky Here</div> */}
