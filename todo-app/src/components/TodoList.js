357dd1b2ef108e2897ba9b54862b40fd83fb9dd8
nmuceros <nolita.muceros@gmail.com>
2021-09-24 00:33:05 -0400

    cleans-up code

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,46 +2,25 @@ import React, { useState } from "react";
 import Todo from "./Todo";
 import "../../src/App.css"
 
-// import deleteIcon from '../images/delete.svg';
-
 const TodoList = () => {
-    /* Hooks */
     const [todo, setTodo] = useState(''); 
     const [todos, setTodos] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    // const validation = value => {
-    //     let errors='';
-        
-    //     if(!todo.entered_todo) {
-    //         errors="Please enter a new task!";
-    //     }
-    //     return errors;
-    // }
 
     const changeTodo_Handler = (e) => {
         setTodo(e.target.value);
         console.log(e.target.value)
         setErrorMessage(!errorMessage)
-
-        // setErrors(!errors.todo)
-        // setTodo((prevState) => ({
-        //     ...prevState, 
-        //     [e.target.name]: e.target.value,
-        // }));
     };
 
     const submitTodo_Handler = (e) => {
         e.preventDefault(); /* prevents state to return to its default state */
-        // setErrors(validation(e.target.value));
+
         if(!todo) {
             setErrorMessage("No task has been entered!")
-
         }
         else {
-
-        
- 
             setTodos([...todos, {
                 entered_todo: todo, 
                 completed: false, 
@@ -50,17 +29,12 @@ const TodoList = () => {
             
             }]);
             setTodo(""); /* set the state to its default value */
-          
         }
     }
-    
-    
 
     return (
         <div className="todo-list-container">
-       
             <div className="form-container">
-             
                 <form onSubmit={ submitTodo_Handler }>
                     <input 
                         className="input-todo" 
@@ -80,30 +54,19 @@ const TodoList = () => {
                 <ul className="todo-list">
                     {todos.map((todo) => 
                         <div key={todo.id}>
-
                                 <Todo 
                                     id={todo.id} 
                                     entered_todo={todo.entered_todo}
                                     todos={todos}
                                     setTodos={setTodos}
                                     todo={todo}
-                                   
                                 />
-                                
-                            
                         </div>
-                        
                     )}
-
-                    
                 </ul>
              </div>
-
-
-
         </div>
     );
-    
 };
 
 export default TodoList;
