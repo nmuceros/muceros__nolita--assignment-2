056e825bf7ecbfdcd8835cbea1ec2c2a91896a02
nmuceros <nolita.muceros@gmail.com>
2021-09-25 20:55:26 -0400

    adds drag and drop animation

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -39,10 +39,36 @@ const TodoList = () => {
     }
 
 
-    const onEnd = result => {
-        console.log(result);
-        setTodos(reOrder(todos, result.source.index, result.destination.index));
+    const onDragEnd_Handler = result => {
+        console.log(`Result is ${result}`);
+        // console.log(`Result.destination.index is ${result.destination.index}`);
+
+        if (result.destination !== null) {
+            setTodos(reOrder(todos, result.source.index, result.destination.index));
+        } 
     }
+    
+    function getStyle(style, snapshot) {
+        if (!snapshot.isDropAnimating) {
+          return style;
+        }
+        const { moveTo, curve, duration } = snapshot.dropAnimation;
+        // move to the right spot
+        const translate = `translate(${moveTo.x}px, ${moveTo.y}px)`;
+        // add a bit of turn for fun
+        const rotate = 'rotate(0.5turn)';
+      
+        // patching the existing style
+        return {
+          ...style,
+          transform: `${translate} ${rotate}`,
+          // slowing down the drop because we can
+          transition: `all ${curve} ${duration + 1}s`,
+        };
+      }
+      
+     
+
 
     return (
         <main className="todo-list-container">
@@ -63,57 +89,72 @@ const TodoList = () => {
 
             {errorMessage && <p className="error">{errorMessage}</p>}
 
-            <DragDropContext onDragEnd={onEnd}>   
 
-                <Droppable droppableId='12345678'>
 
-                    {(provided, snapshot) => (
-                        <div ref={provided.innerRef}>
 
                             <div className="todo-container">
-                                <ul className="todo-list">
-                                    {todos.map((todo, index) => (
 
-                                        <Draggable
-                                            draggableId={todo.id}
-                                            key={todo.id}
-                                            index={index}
-                                        >
-                                            {(provided, snapshot) => (
-                                                <div 
-                                                    key={todo.id}
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                >
+                            
+                                <DragDropContext onDragEnd={onDragEnd_Handler}>   
+
+                                    <Droppable droppableId='12345678'>
+
+                                        {(provided, snapshot) => (
+                                            <div ref={provided.innerRef}>
 
 
-                                                    {/* <div key={todo.id}> */}
-                                                            <Todo 
-                                                                id={todo.id} 
-                                                                entered_todo={todo.entered_todo}
-                                                                todos={todos}
-                                                                setTodos={setTodos}
-                                                                todo={todo}
-                                                            />
-                                                    {/* </div> */}
-                                                </div>
-                                            )}   
+                                                <ul className="todo-list">
+                                                    {todos.map((todo, index) => (
 
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
-                                </ul>
-                            </div>                            
-
-
-                        </div>
-                    )}
+                                                        <Draggable
+                                                            className="draggable"
+                                                            draggableId={todo.id}
+                                                            key={todo.id}
+                                                            index={index}
+                                                        >
+                                                            {(provided, snapshot) => (
+                                                                <div 
+                                                                    key={todo.id}
+                                                                    ref={provided.innerRef}
+                                                                    {...provided.draggableProps}
+                                                                    {...provided.dragHandleProps}
+                                                                    style={getStyle(provided.draggableProps.style, snapshot)}
+                                                                >
 
 
+                                                                    {/* <div key={todo.id}> */}
+                                                                            <Todo 
+                                                                                id={todo.id} 
+                                                                                entered_todo={todo.entered_todo}
+                                                                                todos={todos}
+                                                                                setTodos={setTodos}
+                                                                                todo={todo}
+                                                                            />
+                                                                    {/* </div> */}
+                                                                </div>
+                                                            )}   
 
-                </Droppable>                            
-             </DragDropContext>
+                                                        </Draggable>
+                                                    ))}
+                                                    {provided.placeholder}
+                                                </ul>
+
+                                    
+                                             </div>
+                                        )}
+
+
+
+                                    </Droppable>                            
+                                </DragDropContext>                                        
+
+
+                            </div>    
+
+                            
+                                                    
+
+
 
         </main>
     );
