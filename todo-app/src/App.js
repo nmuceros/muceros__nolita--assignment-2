615cd85961d7756ad20c3443a2dc77636ae13db8
nmuceros <nolita.muceros@gmail.com>
2021-09-29 23:21:11 -0400

    adds box-shadown on main container

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import time from './images/time.png';
 
 function App() {
   return (
-    <div className="main-container">
-       <meta name="viewport" content="width-device-width, initial-scale=1.0" />
-      <h2>My To-Do List</h2>
-         <TodoList />
+    <div className="main">
+        <head>
+          <meta name="viewport" content="width-device-width, initial-scale=1.0" />
+        </head>
+        <h2>My To-Do List</h2>
+        {/* <div className="time-container">
+            <img src={time}/>
+        </div> */}
+        <TodoList />
   </div>
   );
 };
